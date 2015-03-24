65a17306d94bf4bc15275e011a019543b7fca39b
quvung <quangngmetro@gmail.com>
2015-03-24 16:29:45 +0200

    Add code for remote control device

---
+++ remote-control/remote-control.ino
@@ -0,0 +1,22 @@
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
+
+
+
+#define BUTTON_LOCK 4
+#define BUTTON_UNLOCK 5
+#define BUTTON_OPEN 6
+#define BUTTON_CLOSE 7
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
