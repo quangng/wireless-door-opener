065ce57da267d431205c8be5195ae8654bc2e4ef
quvung <quangngmetro@gmail.com>
2015-04-07 01:54:26 +0300

    Added code for remote-control with new block cipher and nrf24l01 libraries

---
+++ remote-control/CPS_AES128/cps_aes128.h
@@ -0,0 +1,16 @@
+#ifndef CPS_AES128_H
+#define CPS_AES128_H
+
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void aes_encrypt(uint8_t *state, uint8_t *key);
+void aes_decrypt(uint8_t *state, uint8_t *key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
